2f9e522d590b0baa58fd702f96739e5260b4ed5e
darwin-prasetio <prasetiodarwin@gmail.com>
2016-02-28 18:12:22 +0700

    completed FileReader

--- src/loader/ReadFile.java
+++ src/loader/ReadFile.java
@@ -1,6 +1,10 @@
 package loader;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
 
 /**
  * Created by Pavilion on 27/02/2016.
@@ -11,10 +15,10 @@ public class ReadFile {
     private final String FOLDER="dataset/";
     private String TrainDataSet;
     private String TestDataSet;
-    private String[] instances;
+    private Vector<String> instances;
 
     public ReadFile() {
-
+        instances = new Vector<String>();
     }
 
     public void setTrainDataSet(String dataSetName) {
@@ -23,6 +27,21 @@ public class ReadFile {
 
     public void parseFile() {
         BufferedReader reader = null;
-        
+        String line="";
+        String splitter=",";
+        try {
+            reader = new BufferedReader(new FileReader(TrainDataSet));
+            while((line = reader.readLine())!=null) {
+                instances.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Vector<String> getInstances() {
+        return instances;
     }
 }
